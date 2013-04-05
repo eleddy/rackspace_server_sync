83c2a3d29be3ddb57a654484345272ede2a1f863
James Sutterfield <james.sutterfield@gmail.com>
2013-04-04 19:39:16 -0700

    working version of script

--- sync_rackspace.py
+++ sync_rackspace.py
@@ -2,10 +2,11 @@ import ConfigParser
 import requests
 import json
 import sys
+import os
 
 LIST_SERVERS_URI = '/servers/detail'
 
-# Read the rackspace api credentials form the connect.cfg file
+# Read the rackspace api credentials from the connect.cfg file
 config = ConfigParser.RawConfigParser()
 try:
     fp = open('connect.cfg')
@@ -30,7 +31,8 @@ except (ConfigParser.NoOptionError, ConfigParser.NoSectionError):
 
 # Get AUTH-TOKEN
 token_url = 'https://identity.api.rackspacecloud.com/v2.0/tokens'
-payload = {"auth":{"RAX-KSKEY:apiKeyCredentials":{"username":"%s" % username, "apiKey":"%s" % api_key}}}
+payload = {"auth":{"RAX-KSKEY:apiKeyCredentials":
+          {"username":"%s" % username, "apiKey":"%s" % api_key}}}
 headers = {'content-type': 'application/json'}
 request = requests.post(token_url, data=json.dumps(payload), headers=headers)
 token = request.json()['access']['token']['id']
@@ -41,9 +43,9 @@ for server_dict in request.json()['access']['serviceCatalog']:
     if server_dict['name'] == 'cloudServers':
         firstgen_server_api_url = server_dict['endpoints'][0]['publicURL']
         break
-
 headers = {"X-Auth-Token": "%s" % token}
-firstgen_request = requests.get(firstgen_server_api_url + LIST_SERVERS_URI, headers=headers)
+firstgen_request = requests.get(firstgen_server_api_url + LIST_SERVERS_URI, 
+                                headers=headers)
 first_gen_json = firstgen_request.json()
 
 # Get Server info for second generation servers
@@ -52,28 +54,51 @@ for server_dict in request.json()['access']['serviceCatalog']:
     if server_dict['name'] == 'cloudServersOpenStack':
         secondgen_server_api_url = server_dict['endpoints'][0]['publicURL']
         break
-
 headers = {"X-Auth-Token": "%s" % token}
-secondgen_request = requests.get(secondgen_server_api_url + LIST_SERVERS_URI, headers=headers)
+secondgen_request = requests.get(secondgen_server_api_url + LIST_SERVERS_URI,
+                                 headers=headers)
 second_gen_json = secondgen_request.json()
 
 # Create list of all server names and ips
 server_info = []
 for server in first_gen_json['servers']:
-    server_info.append(server['name'], ['server']['addresses']['public'][0])
+    server_info.append((server['name'], server['addresses']['public'][0]))
+for server in second_gen_json['servers']:
+    server_info.append((server['name'], server['accessIPv4']))
 
+# Get information from the ect/hosts file
+old_lines = []
+with open('/etc/hosts') as fp:
+    for line in fp.readlines():
+        # Check to see if the line is just a newline character
+        try:
+            ip = line.split()[0]
+        except IndexError:
+            old_lines.append(line)
+            continue
+        for server in server_info:
+            if server[0] == ip:
+                # If the ip address is already in the hosts file, and it has the alias from
+                # rackspace associated with it, remove the ip/alias info from the server_info
+                # lise and continue
+                if server[1] in line.split():
+                    server_info.pop(server_info.index(server))
+                    continue
+                # If it doesn't have the alias from rackspace associated with it, add that name
+                else:
+                    line = line + " %s" % server[1]
+                    continue
+        else:
+            old_lines.append(line)
 
-import pdb; pdb.set_trace()
-x['servers'][0]['name']
-x['servers'][0]['addresses']['public'][0]
+# Write to a temporary file
+with open ('/tmp/new_hosts.tmp', 'wb') as fp:
+    for line in server_info:
+        fp.write('%s\t%s\n' % (line[0], line[1]))
+        print 'writing %s\t%s\n' % (line[0], line[1])
+    for line in old_lines:
+        fp.write(line)
+        print 'writing %s' % line
 
-
-print "FIRST GEN SERVERS"
-print "=" * 80
-print first_gen_json
-
-print '\n' * 2
-
-print 'SECOND GEN SERVERS'
-print "=" * 80
-print second_gen_json
+# Overwrite the /etc/hosts file with the new information
+os.system('sudo mv /tmp/new_hosts.tmp /etc/hosts')
